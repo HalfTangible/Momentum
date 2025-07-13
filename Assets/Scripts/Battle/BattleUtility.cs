3463bea88d24116fa9ee3451ef53d93089dbb473
HalfTangible <HTNetherwing@gmail.com>
2025-07-12 23:41:41 -0500

    Started NextToFight method

--- Assets/Scripts/Battle/BattleUtility.cs
+++ Assets/Scripts/Battle/BattleUtility.cs
@@ -37,4 +37,31 @@ public static class BattleUtility
                 sheet.ApplyRoundEffects();
         }
     }
+
+    public static StatSheet NextToFight(List<StatSheet> allParticipants)
+    {
+        //Sort all the StatSheets based on the Momentum of the participants.
+        //Return the character with the highest momentum.
+        //Actually, sorting them isn't necessary if you're just returning whoever's next.
+        
+        StatSheet nextToFight = allParticipants[0];
+        /*
+        for(int i = 1; i < allParticipants.Count; i++)
+        {
+            if (!nextToFight.isAlive())
+                nextToFight = allParticipants[i];
+            else
+                if (nextToFight.momentum.Current < allParticipants[i].momentum.Current)
+                nextToFight = allParticipants[i];
+        }*/
+
+        //Very inefficient
+        //Get a list for the player party and the enemy party
+        //Sort those two lists, with the dead at the very back, then compare the first person in each list.
+        //It's a bit more annoying to code but it means we only have to sort the entirety of each array *once*.
+
+        return nextToFight;
+
+
+    }
 }
